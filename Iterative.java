fbcf6f76d1631286ea1f28326dc5ab17dec0c2b8
SparrowJS <gmoney510@outlook.com>
2014-05-03 17:25:07 -0500

    Update Iterative.java

--- Iterative.java
+++ Iterative.java
@@ -1,4 +1,13 @@
 
 public class Iterative {
 
+  // I am soo proud of you for realising that this is a simple factorial method
+  public long fact(long n) {
+    if (n == 1) {
+      return 1;
+    } else {
+      return n * fact(n - 1);
+    }
+  }
+
 }
